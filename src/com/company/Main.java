b4dd5b68be2ef709eab19de3928238f51d22da6f
Noman <alter.noman@gmail.com>
2018-11-01 15:02:31 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	StragedyUsage str = new StragedyUsage();
-	str.setStrategyUsage(new Strategy1());
+	StragedyUsage str = new StragedyUsage(new Strategy1());
+	//str.setStrategyUsage(new Strategy1());
 	str.useStragedy();
 
     str.setStrategyUsage(new Strategy2());
--- src/com/company/StragedyUsage.java
+++ src/com/company/StragedyUsage.java
@@ -3,6 +3,10 @@ package com.company;
 public class StragedyUsage {
     private Strategy strategy;
 
+    public StragedyUsage(Strategy strategy){
+        this.strategy = strategy;
+    }
+
     public void setStrategyUsage (Strategy strategy){
         this.strategy = strategy;
     }
