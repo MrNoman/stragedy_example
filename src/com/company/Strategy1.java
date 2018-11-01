1df9730ddee731089728a9fff5ca6f2407bc41cc
Noman <alter.noman@gmail.com>
2018-11-01 13:38:30 +0300

    .

--- src/com/company/Strategy1.java
+++ src/com/company/Strategy1.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Strategy1 implements Strategy {
-    @Override
     public void doSmth() {
         System.out.println("метод 1");
     }
--- src/com/company/Strategy2.java
+++ src/com/company/Strategy2.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Strategy2 implements Strategy{
-    @Override
     public void doSmth() {
         System.out.println("метод 2");
     }
